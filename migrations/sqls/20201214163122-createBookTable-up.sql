bdf8da2d42cf7140dd605d9478c9cf273680d255
toto <torphi78@gmail.com>
2020-12-16 14:18:17 +0100

    1st commit

--- migrations/sqls/20201214163122-createBookTable-up.sql
+++ migrations/sqls/20201214163122-createBookTable-up.sql
@@ -2,8 +2,8 @@ CREATE TABLE `Book` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `Title` varchar(80) NOT NULL,
   `Description` TEXT NOT NULL,
-  `Price` varchar(20) INT NOT NULL,
-  `Publication` Date,
+  `Price` INT NOT NULL,
+  `Publication` date,
   PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
