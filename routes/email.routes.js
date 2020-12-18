530bd33da716537b1ee39b2506a6cc3f27624615
toto <torphi78@gmail.com>
2020-12-18 14:28:39 +0100

    add Email routes

--- routes/email.routes.js
+++ routes/email.routes.js
@@ -1,11 +1,84 @@
 const router=require('express').Router();
+const {connection}=require('../db_connection');
 
-router.get('/', (req, res)=> {
+router.get("/", (req, res) => {
+    const sql='SELECT * FROM Email';
+    connection.query(sql, (err, result)=>{
+        if (err){
+            res.status(500).json({errorMessage: err.message});
+        }
+        else{
+            res.status(200).json(result);
+        }
+    })    
+  });
 
-    res.sendStatus(200);
+  router.get("/:id", (req, res) => {
+    connection.query(
+      "SELECT * from Email WHERE id=?",
+      [req.params.id],
+      (err, results) => {
+        if (err) {  
+          console.log(err);
+          res.status(500).send("Error receiving data");
+        } else {
+          res.status(200).json(results);
+        }
+      }
+    );
+  });
 
+  router.post("/", (req, res) => {
+    const { Email } = req.body;
+    if (!Email) {
+      res.status(400).json({ errorMessage: "All elements required"});
+    } else {
+      connection.query("SELECT * FROM Email WHERE Email = ?",
+      [Email],
+      (errOne, resultOne) => {
+          if(errOne || resultOne.length>0) {
+            res.status(500).json({ errorMessage: "this Email already exists"});
+          }
+          else {
+            connection.query("INSERT INTO Email (Email) VALUES(?)",
+                [Email],
+                (errTwo, resultTwo) => {
+                  if (errTwo) {
+                    console.log(errTwo);
+                    res.status(500).send("Error saving an email");
+                  } else {
+                    connection.query('SELECT * FROM Email WHERE id=?', [resultTwo.insertId], (errThree, resultThree)=> {
+                      if (errThree) {
+                        console.log(errThree);
+                        res.status(500).send(errThree.message);
+                      }
+                      else {
+                        res.status(201).json(resultThree[0]);
+                      }                    
+                    })                    
+                  }
+                })
+          }
+      })
+    }
+  });
+  
 
-}) 
+  router.delete("/:id", (req, res) => {
+    const idEmail = req.params.id;
+    connection.query(
+      "DELETE FROM Email WHERE id = ?",
+      [idEmail],
+      (err) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send("😱 Error deleting an email");
+        } else {
+          res.status(200).send("🎉 the email deleted!");
+        }
+      }
+    );
+  });
 
 
 module.exports=router;
--- tests/emails.endpoints.test.js
+++ tests/emails.endpoints.test.js
@@ -21,14 +21,23 @@ describe('emails', () => {
     // expect(parseInt(res.body.length, 10)).toEqual(0);
   });
 
-//   it('POSTs /api/pictures and should return {name: "Honda_civic_9.jpg",description: "Best car in the world", dimension: 4000}', async () => {
-//     const res = await request(app)
-//       .post('/api/pictures')
-//       .send(picture)
-//       .expect(201);
-//     expect(res.body.id).toBe(1);
-//     expect(res.body.name).toEqual("Honda_civic_9.jpg");
-//   });
+  it('POSTs /api/emails and should return the email created', async () => {
+    const res = await request(app)
+      .post('/api/emails')
+      // .send("Email")
+      .expect(201);
+    expect(res.body.id).toBe(1);
+    // expect(res.body.email).toEqual("fushi@gmail.com");
+  });
+
+  // it('POSTs /api/pictures and should return {name: "Honda_civic_9.jpg",description: "Best car in the world", dimension: 4000}', async () => {
+  //   const res = await request(app)
+  //     .post('/api/pictures')
+  //     .send(picture)
+  //     .expect(201);
+  //   expect(res.body.id).toBe(1);
+  //   expect(res.body.name).toEqual("Honda_civic_9.jpg");
+  // });
 
 //   it('GETs /api/pictures and should return an array containing one element', async () => {
 //     const res = await request(app)
