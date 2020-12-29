00244906f3472c2c10dfc7d8c00c985cabd9031a
Sarah Q <sarahquero@gmail.com>
2020-12-29 19:48:11 +0100

    added image routes

--- routes/image.routes.js
+++ routes/image.routes.js
@@ -2,9 +2,9 @@ const router=require('express').Router();
 const { connection }=require('../db_connection');
 
 
-//GET all of the Images from your table
+// GET all of the Images from your table
 router.get("/", (req, res) => {
-    let sql = 'SELECT * FROM Image'
+    const sql = 'SELECT * FROM Image'
     connection.query(sql, (err, results) => {
         if (err) {
             console.log(err);
@@ -21,14 +21,13 @@ router.get("/:id", (req, res) => {
     connection.query(`SELECT * FROM Image WHERE id = ?`, [req.params.id], (err, results) => {
         if (results.length === 0 || results === undefined) {
             res.status(404).json( { error: "User not found"});
-        } else {
-            if (err) {
+        } else if (err) {
                 res.status(500).send("Error retrieving a user");
-            } 
-            const selectedImage = results[0];
-        const { Description, ...Image } = selectedImage;
-        return res.status(200).json(Image);
-        }
+            } else {
+                const selectedImage = results[0];
+                const { Description, ...Image } = selectedImage;
+                res.status(200).json(Image);
+            }
     });
 });
 
@@ -57,7 +56,7 @@ router.post('/', (req, res) => {
 router.put("/:id", (req, res) => {
     const idImage = req.params.id;
     const updatedImage = req.body;
-    return connection.query('UPDATE Image SET ? WHERE id = ?', [updatedImage, idImage], (err, results) => {
+    return connection.query('UPDATE Image SET ? WHERE id = ?', [updatedImage, idImage], (err) => {
         if (err) {
             return res.status(500).json({
                 error: err.message,
@@ -79,7 +78,7 @@ router.put("/:id", (req, res) => {
 // DELETE Delete an image
 router.delete("/:id", (req, res) => {
     const idImage = req.params.id;
-    connection.query('DELETE FROM Image WHERE id = ?', [idImage], (err, results) => {
+    connection.query('DELETE FROM Image WHERE id = ?', [idImage], (err) => {
         if (err) {
             res.status(500).send("Error deleting an image");
         } else {
