65964bb4df4499e9e937313d268979bc3f0f5ade
Sarah Q <sarahquero@gmail.com>
2020-12-30 16:36:25 +0100

    tests for image route completed

--- tests/setupTests.js
+++ tests/setupTests.js
@@ -1,6 +1,25 @@
-const { closeConnection } = require('../db_connection.js');
+const {closeConnection} = require('../db_connection.js');
 const app = require('../app.js');
 
+// const deleteAllData = async () => {
+//   await deleteAllDBData();
+// };
+
+// const closeApp = () =>
+//   new Promise((resolve, reject) => {
+//     app.close((err) => {
+//       if (err) reject(err);
+//       else resolve();
+//     });
+//   });
+
+// beforeAll(deleteAllData);
+// afterAll(async () => {
+//   await closeConnection();
+//   await closeApp();
+// });
+
+//Current
 const closeApp = () =>
   new Promise((resolve, reject) => {
     app.close((err) => {
