9f3089b81519d2c0d913bcabc231798ae88be01a
Sarah Q <sarahquero@gmail.com>
2021-01-02 11:00:08 +0100

    updating setupTests.js file

--- tests/setupTests.js
+++ tests/setupTests.js
@@ -1,25 +1,6 @@
 const {closeConnection} = require('../db_connection.js');
 const app = require('../app.js');
 
-// const deleteAllData = async () => {
-//   await deleteAllDBData();
-// };
-
-// const closeApp = () =>
-//   new Promise((resolve, reject) => {
-//     app.close((err) => {
-//       if (err) reject(err);
-//       else resolve();
-//     });
-//   });
-
-// beforeAll(deleteAllData);
-// afterAll(async () => {
-//   await closeConnection();
-//   await closeApp();
-// });
-
-//Current
 const closeApp = () =>
   new Promise((resolve, reject) => {
     app.close((err) => {
