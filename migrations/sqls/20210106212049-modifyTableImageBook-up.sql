6a3f84b5ae99840947e43574e394c82e6c839e34
toto <torphi78@gmail.com>
2021-01-07 16:46:30 +0100

    add Funding Route

---
+++ migrations/sqls/20210106212049-modifyTableImageBook-up.sql
@@ -0,0 +1,8 @@
+ALTER TABLE Image
+DROP COLUMN `Description`;
+ALTER TABLE Image
+DROP COLUMN `Device`;
+ALTER TABLE Book
+ADD `Link` varchar(255);
+
+
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router=require('express').Router();
 const emailRoutes = require('./email.routes');
+const fundingRoutes = require('./funding.routes');
 
 router.use('/emails', emailRoutes);
+router.use('/fundings', fundingRoutes);
 
 module.exports = router;
