e122a4326744dea035c950e171fc7170234ffc73
Sarah Q <sarahquero@gmail.com>
2021-01-11 14:23:10 +0100

    start upload image route

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ const imageRoutes = require('./image.routes');
 const useradminRoutes = require('./useradmin.routes');
 const livreRoutes = require('./livre.routes');
 const fundingRoutes = require('./funding.routes');
+const uploadRoutes = require('./upload.routes');
 
 router.use('/emails', emailRoutes);
 router.use('/images', imageRoutes);
 router.use('/useradmin', useradminRoutes);
 router.use('/livres', livreRoutes);
 router.use('/fundings', fundingRoutes);
+router.use('/upload', uploadRoutes);
 
 module.exports = router;
